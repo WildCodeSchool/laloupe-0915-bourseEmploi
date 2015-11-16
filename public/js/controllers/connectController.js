bf36c1858b4307d4ec83d38fc4d4925f00f3a590
Rabin Julien <julienr114@gmail.com>
2015-11-16 18:55:43 +0100

    responsiv page d'accueil

--- public/js/controllers/connectController.js
+++ public/js/controllers/connectController.js
@@ -3,17 +3,29 @@ function connectController($scope, $rootScope, $location, connectService){
     $scope.checkedR = function(){
         $scope.loginR = !$scope.loginR;
         $scope.loginE = false;
-        
+        if($scope.loginR == true || $scope.loginE == true){
+            $scope.hideAccueil = true;
+        } else {
+             $scope.hideAccueil = false;
+        }
     }
     
     $scope.checkedE = function(){
         $scope.loginE = !$scope.loginE;
         $scope.loginR = false;
+        if($scope.loginR == true || $scope.loginE == true){
+            $scope.hideAccueil = true;
+        } else {
+             $scope.hideAccueil = false;
+        }
     }
     
     $scope.connectSignUpRecruiter = function(){
         $location.path('/connectRecruiter');
     }
+        
+    
+    
     
 	$scope.connect = function(){
 		connectService.connect($scope.user).then(function(res){
