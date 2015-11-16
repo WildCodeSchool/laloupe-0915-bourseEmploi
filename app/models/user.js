50b25e147683965097c38b5f51351a5b79008350
Arano <etsaiak2@hotmail.fr>
2015-11-16 09:28:30 +0100

    debut de base de donné

--- app/models/user.js
+++ app/models/user.js
@@ -1,58 +1,66 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  password: String,
-  admin: Boolean
+ name: { type: String, required: true, unique: true },
+ first_name: String,
+ photo: Text,
+ age: Number,
+ description: String,
+ wild side: String,
+ training: String,
+ previous work: String,
+ skill: String, 
+ email: String,
+ tel: Number
 });
 
 var User = {
-    model: mongoose.model('User', userSchema),
-    
-    find: function(name, password, callback) {
-        User.model.findOne({
-            name: name,
-            password: password
-		}, callback);
-	},
-    
-    findAll: function(req, res) {
-		User.model.find({}, function (err, users) {
-			res.json(users);
-		});
-	},
+   model: mongoose.model('User', userSchema),
+   
+   find: function(name, password, callback) {
+       User.model.findOne({
+           name: name,
+           password: password
+        }, callback);
+    },
+   
+   findAll: function(req, res) {
+        User.model.find({}, function (err, users) {
+            res.json(users);
+        });
+    },
 
-	findById: function(req, res) {
-		User.model.findById(req.headers.id, function (err, user) {
-			 res.json(user);
-		});
-	},
+    findById: function(req, res) {
+        User.model.findById(req.headers.id, function (err, user) {
+             res.json(user);
+        });
+    },
 
-	create: function(req, res) {
-		User.model.create({
-			name: req.body.name,
-			password: req.body.password,
-			admin: req.body.admin
-		}, function(err, user) {
-			res.json(user);
-	    });
-	},
+    create: function(req, res) {
+        User.model.create({
+            name: req.body.name,
+            password: req.body.password,
+            admin: req.body.admin
+        }, function(err, user) {
+            res.json(user);
+        });
+    },
 
-	update: function(req, res) {
-		User.model.findByIdAndUpdate(req.params.id, {
-			name: req.body.name,
-			password: req.body.password,
-			admin: req.body.admin
-		}, function(err, user) {
-			res.json(user);
-	    });
-	},
+    update: function(req, res) {
+        User.model.findByIdAndUpdate(req.params.id, {
+            name: req.body.name,
+            password: req.body.password,
+            admin: req.body.admin
+        }, function(err, user) {
+            res.json(user);
+        });
+    },
 
-	delete: function(req, res){
-		User.model.findByIdAndRemove(req.params.id, function(){
-			res.sendStatus(200);
-		})
-	}
+    delete: function(req, res){
+        User.model.findByIdAndRemove(req.params.id, function(){
+            res.sendStatus(200);
+        })
+    }
 }
 
 
