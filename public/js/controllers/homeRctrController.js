a06ce9529fb793b948f2c1791ae8ba704fc8ef05
Rabin Julien <julienr114@gmail.com>
2015-11-18 14:27:32 +0100

    page homeRecruiter terminé aux niveau du front

--- public/js/controllers/homeRctrController.js
+++ public/js/controllers/homeRctrController.js
@@ -1,4 +1,25 @@
 function homeRctrController($scope){
 
+$scope.currentoffers = true;
+$scope.comingoffers = false;
+$scope.expiredoffers = false;
+    
+$scope.show1 = function(){
+    $scope.currentoffers = true;
+    $scope.comingoffers = false;
+    $scope.expiredoffers = false;    
+}
+
+$scope.show2 = function(){
+    $scope.currentoffers = false;
+    $scope.comingoffers = true;
+    $scope.expiredoffers = false;    
+}
+
+$scope.show3 = function(){
+    $scope.currentoffers = false;
+    $scope.comingoffers = false;
+    $scope.expiredoffers = true;    
+}
 
 }
