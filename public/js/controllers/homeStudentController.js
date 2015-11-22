cd22afc4af16371a699028f6c9dafa23ed0605f5
Rabin Julien <julienr114@gmail.com>
2015-11-22 16:12:50 +0100

    front page offer terminé, modification dans les listes qui sont maintenant en display block, google map dans les offres et dans homeStudient

--- public/js/controllers/homeStudentController.js
+++ public/js/controllers/homeStudentController.js
@@ -1,4 +1,14 @@
 function homeStudentController($scope){
+    
+    function initialize() {
+      var mapProp = {
+        center:new google.maps.LatLng(46.8429639,2.1696329),
+        zoom:5,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 
 
 }
