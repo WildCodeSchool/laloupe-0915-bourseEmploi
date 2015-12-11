9d95b1a5a47d671d2cea399a815cfb757373728f
Rabin Julien <julienr114@gmail.com>
2015-12-11 16:33:08 +0100

    ajout des marker dans les cartes de la page searchoffer

--- public/js/controllers/searchOfferController.js
+++ public/js/controllers/searchOfferController.js
@@ -1,8 +1,8 @@
-function searchOfferController($scope) {
+function searchOfferController($scope, offerService, geocoderService) {
 
-    //MAP
     $scope.showMap = true;
 
+    //MAP
     L.mapbox.accessToken = 'pk.eyJ1IjoianVsaWVucjExNCIsImEiOiJjaWhobXZ2eHYwMGFxdTJtNDhuNW5xMjBxIn0.KkUadZFGBKA1ENyPLDTxjg';
     var map = L.mapbox.map('map', 'mapbox.streets')
         .setView([46.84, 2.00], 5);
@@ -10,6 +10,27 @@ function searchOfferController($scope) {
     L.mapbox.accessToken = 'pk.eyJ1IjoianVsaWVucjExNCIsImEiOiJjaWhobXZ2eHYwMGFxdTJtNDhuNW5xMjBxIn0.KkUadZFGBKA1ENyPLDTxjg';
     var map2 = L.mapbox.map('map2', 'mapbox.streets')
         .setView([46.84, 2.00], 5);
+
+    function loadOffer() {
+        offerService.getAll().then(function (res) {
+            $scope.offers = res.data;
+
+            //MARKERS
+            $scope.offers.forEach(function (offer) {
+                var address = offer.address + ", " + offer.zipCode + " " + offer.city + ", " + offer.country;
+
+                geocoderService.CoordinateByAdress(address).then(function (res) {
+                    var lng = res.data.features[0].geometry.coordinates[0];
+                    var lat = res.data.features[0].geometry.coordinates[1];
+
+                    var marker = new L.marker([lat, lng]).addTo(map);
+                    var marker2 = new L.marker([lat, lng]).addTo(map2);
+                });
+            });
+        });
+    }
+
+    loadOffer();
 
     //TOOLTIP    
     $(function () {
