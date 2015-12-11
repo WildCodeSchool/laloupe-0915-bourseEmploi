4e9fb98b06068e6fc1e68650464f110e1df7ff67
noaa18 <noaa18@orange.fr>
2015-12-11 16:04:51 +0100

    mise a jour des ngmodel pour edit offer et ajout du editoffercontroller

--- public/js/controllers/editOfferController.js
+++ public/js/controllers/editOfferController.js
@@ -1,3 +1,109 @@
-function editOfferController($scope, $location, offerService) {
+function editOfferController($scope, $location, offerService, skillService, $rootScope) {
+
+    var user = $rootScope.user.skills;
+    console.log(user);
+
+
+    //Mise a jour infos personnelles
+    $scope.update1 = function () {
+        var data = {};
+        data.referentName = $scope.referentName;
+        data.referentEmail = $scope.referentEmail;
+        data.referentPhone = $scope.referentPhone;
+        offerService.update(data).then(function (res) {
+            if (!res.data) {} else {
+
+            }
+        })
+    }
+
+    //Mise a jour coordonnées
+    $scope.update2 = function () {
+        var data = {};
+        data.country = $scope.country;
+        data.city = $scope.city;
+        data.zipCode = $scope.zipCode;
+        data.address = $scope.address;
+        offerService.update(data).then(function (res) {
+            if (!res.data) {} else {
+
+            }
+        })
+    }
+
+    //Mise a jour détail offre
+    $scope.update3 = function () {
+        var data = {};
+        data.title = $scope.title;
+        data.contract = $scope.contract;
+        data.experience = $scope.experience;
+        data.salary = $scope.salary;
+        offerService.update(data).then(function (res) {
+            if (!res.data) {} else {
+
+            }
+        })
+    }
+
+
+    /****   CREATION TAGS ******/
+    //Import des compétences de shéma "skills"
+    skillService.get().then(function (res) {
+        $scope.offerSkills = res.data;
+    });
+
+    //Init de la fonctionnalitée
+    $scope.showSkill = false;
+    var dataSkill = [];
+    $scope.listSkills = dataSkill;
+
+    //Vérification et ajout de la valeur de l'input
+    function updateSkill(array, up) {
+        $scope.errorTyping = true;
+        $scope.offerSkills.forEach(function (skill) {
+            //console.log(skill.title);
+            if (up == skill.title) {
+                $scope.errorTyping = false;
+            }
+        });
+        if (!$scope.errorTyping && dataSkill.indexOf(up) === -1) {
+            dataSkill.push(up);
+            console.log('Le nouveau tableau est : ' + dataSkill);
+        } else if (dataSkill.indexOf(up) > -1) {
+            $scope.errorChoice = true;
+        }
+    }
+
+    //Lancement de la fonction  precedente au clic
+    $scope.add = function () {
+        $scope.errorChoice = false;
+        $scope.errorTyping = false;
+        var up = $scope.chooseSkill.toUpperCase();
+        console.log($scope.chooseSkill);
+        updateSkill(dataSkill, up);
+        //Affichage du skill et champ vide
+        $scope.showSkill = true;
+        $scope.chooseSkill = "";
+    }
+
+    //Suppression des Tags
+    $scope.deleteSkill = function deleteASkill(id) {
+        var idDeletedSkill = dataSkill.indexOf(id);
+        console.log($scope.listSkills);
+        return dataSkill.splice(idDeletedSkill, 1);
+    }
+
+    //Mise a jour...
+    $scope.update5 = function () {
+        var data = {};
+        data.description = $scope.description;
+        data.responsability = $scope.responsability;
+        data.wildSide = $scope.wildSide;
+        offerService.update(data).then(function (res) {
+            if (!res.data) {} else {
+
+            }
+        })
+    }
 
 }
